ec3b02facfe87a613fa4c0c7916908519893c1a0
Abdullah-Younus <abdullah1001k@gmail.com>
2023-09-03 02:43:48 +0500

    local changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,19 @@
+"use client"
+
 import Link from 'next/link';
 import React from 'react';
 import Logo from './Logo';
+import { usePathname, useRouter } from 'next/navigation';
 
 const CustomLink = ({ href, title, className = "" }: any) => {
+
+    const currentPage = usePathname();
 
     return (
         <Link href={href} className={`${className} relative group`} >
             {title}
 
-            <span className='h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300'>&nbsp;</span>
+            <span className={`${currentPage === href ? "w-full" : "w-0"} h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300`}>&nbsp;</span>
         </Link>
     )
 }
@@ -18,10 +23,10 @@ const NavBar = () => {
     return (
         <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
             <nav>
-                <CustomLink href='/' title="Home" className="mr-4"/>
+                <CustomLink href='/' title="Home" className="mr-4" />
                 <CustomLink href='/about' title="About" className="mx-4" />
-                <CustomLink href='/projects' title="Projects" className="mx-4"/>
-                <CustomLink href='/articles' title="Articles" className="ml-4"/>
+                <CustomLink href='/projects' title="Projects" className="mx-4" />
+                <CustomLink href='/articles' title="Articles" className="ml-4" />
             </nav>
 
             <nav>
