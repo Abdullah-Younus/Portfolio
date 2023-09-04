086b5ac193911dff792272f54540d0cc43d472f8
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-04 21:14:24 +0500

    local changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,39 @@ import Layout from '@/components/Layout';
 import NavBar from '@/components/NavBar';
 import Image from 'next/image';
 import ProfileImage from './../../public/images/profile/developer-pic-1.png';
+import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className='flex items-center text-dark w-full min-h-screen'>
-      <Layout>
+      <Layout className='pt-0'>
         <div className="flex items-center justify-between w-full">
-          <div>
+          <div className='w-1/2'>
             <Image src={ProfileImage} alt='Profile Image' className='w-full h-auto' />
+          </div>
+          <div className='w-1/2 flex flex-col items-center self-center'>
+            <AnimatedText text="Turning Vision Into Reality With Code And Design. " className="!text-6xl !text-left" />
+            <p className='my-4 text-base font-medium'>
+              As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
+              Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+            </p>
+
+            <div className='flex items-center self-start mt-2'>
+              <Link
+                href={"/dummy.pdf"}
+                target='_blank'
+                className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark'
+                download={true}
+              >
+
+                Resume
+              </Link>
+              <Link href={"mailto:abdullah1001k@gmail.com"} target='_blank'>
+                Contact
+              </Link>
+
+            </div>
           </div>
         </div>
       </Layout>
