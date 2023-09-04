1b7911e9a750517751528426ced50a5a76097d37
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-05 00:39:14 +0500

    local changes

--- src/components/Skilled.tsx
+++ src/components/Skilled.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 const Skilled = () => {
   return (
     <div>
-        <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
-          <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
+      <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
+      <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
+        <div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light'>
+            Web
+        </div>
 
-          </div>
-    
+      </div>
+
     </div>
   )
 }
