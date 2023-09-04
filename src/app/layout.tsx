6341f49a87a3e50321d691dc7538a7902f79997a
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-04 19:37:56 +0500

    local chanegs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from '@/components/NavBar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,9 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-
-      <body className={inter.className}>
-          {children}
+      <body className="w-full min-h-screen">
+        <NavBar />
+        {children}
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import NavBar from '@/components/NavBar'
-import Image from 'next/image'
+import Layout from '@/components/Layout';
+import NavBar from '@/components/NavBar';
+import Image from 'next/image';
+import ProfileImage from './../../public/images/profile/developer-pic-1.png';
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen">
-        <NavBar />
+    <main className='flex items-center text-dark w-full min-h-screen'>
+      <Layout>
+        <div className="flex items-center justify-between w-full">
+          <div>
+            <Image src={ProfileImage} alt='Profile Image' className='w-full h-auto' />
+          </div>
+        </div>
+      </Layout>
     </main>
   )
 }
