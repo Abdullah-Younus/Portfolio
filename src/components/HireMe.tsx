ae372ab4ceeb376c773d74dff3eee50589f00eca
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-04 21:52:39 +0500

    footer done

---
+++ src/components/HireMe.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HireMe = () => {
+    return (
+        <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden'>
+            HireMe
+        </div>
+    )
+}
+
+export default HireMe;
