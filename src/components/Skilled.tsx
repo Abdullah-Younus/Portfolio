d3f708d9763a7dce173ba512e59b7bf1188767df
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-05 20:05:27 +0500

    local changes

--- src/components/Skilled.tsx
+++ src/components/Skilled.tsx
@@ -5,7 +5,7 @@ const Skilled = () => {
     <div>
       <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
       <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
-        <div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light'>
+        <div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark'>
             Web
         </div>
         
