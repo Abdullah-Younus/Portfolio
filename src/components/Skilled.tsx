b9c85a16acca775930742723188a8d8442c9bda0
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-05 20:55:02 +0500

    skilled componet done

--- src/components/Skilled.tsx
+++ src/components/Skilled.tsx
@@ -4,7 +4,13 @@ import { motion } from 'framer-motion';
 const Skill = ({ name, x, y }: any) => {
 
   return (
-    <motion.div whileHover={{ scale: 1.05 }} initial={{ x: 0, y: 0 }} whileInView={{ x: x, y: y }} transition={{ duration: 1.5 }} className='flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark absolute'>
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      initial={{ x: 0, y: 0 }}
+      whileInView={{ x: x, y: y }}
+      transition={{ duration: 1.5 }}
+      viewport={{ once: true }}
+      className='flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark absolute'>
       {name}
     </motion.div>
   )
@@ -19,7 +25,7 @@ const Skilled = () => {
         <motion.div whileHover={{ scale: 1.05 }} className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark'>
           Web
         </motion.div>
-        
+
         <Skill name="HTML" x="-25vw" y="2vw" />
         <Skill name="CSS" x="-5vw" y="-10vw" />
         <Skill name="JAVASCRIPT" x="20vw" y="6vw" />
