0ad7486eaae2ec985b7a56fb4ba1ab8cd78a19b0
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-05 22:58:26 +0500

    loal changes

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -6,7 +6,7 @@ const Details = ({ position, company, companyLink, time, address, work }: any) =
     const ref = useRef(null);
     return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
         <LineLiicon reference={ref} />
-        <div>
+        <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }} transition={{ duration: 0.5, type: "spring" }}>
             <h3 className='capitalize font-bold text-2xl'>{position}&nbsp; <a href={companyLink} className='text-primary capitalize' target='_blank'>@{company}</a> </h3>
             <span className='capitalize font-medium text-dark/75'>
                 {time} | {address}
@@ -14,7 +14,7 @@ const Details = ({ position, company, companyLink, time, address, work }: any) =
             <p className='font-medium w-full'>
                 {work}
             </p>
-        </div>
+        </motion.div>
     </li>
 
 }
@@ -37,7 +37,7 @@ const Experience = () => {
 
             <div ref={ref} className='w-[75%] mx-auto relative'>
 
-                <motion.div style={{ scaleY: scrollYProgress }} className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top' />
+                <motion.div style={{ scaleY: scrollYProgress }} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top' />
 
                 <ul className='w-full flex flex-col items-start justify-between ml-4'>
                     <Details
