69d560bad1f3c571e5189435eae311ab41a1f3c5
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-09 05:09:57 +0500

    local changes

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -37,7 +37,7 @@ const Education = () => {
 
             <div ref={ref} className='w-[75%] mx-auto relative'>
 
-                <motion.div style={{ scaleY: scrollYProgress }} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top' />
+                <motion.div style={{ scaleY: scrollYProgress }} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light' />
 
                 <ul className='w-full flex flex-col items-start justify-between ml-4'>
                     <Details
@@ -46,23 +46,23 @@ const Education = () => {
                         place="Massachusetts Institute Of Technology (MIT)"
                         info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
                         Intelligence."
-                      
+
                     />
-                     <Details
+                    <Details
                         type="Bachelor Of Science In Computer Science"
                         time="2016-2020"
                         place="Massachusetts Institute Of Technology (MIT)"
                         info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
                         Intelligence."
-                      
+
                     />
-                     <Details
+                    <Details
                         type="Bachelor Of Science In Computer Science"
                         time="2016-2020"
                         place="Massachusetts Institute Of Technology (MIT)"
                         info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
                         Intelligence."
-                      
+
                     />
                 </ul>
             </div>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,8 +7,8 @@ const Details = ({ position, company, companyLink, time, address, work }: any) =
     return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
         <LineLiicon reference={ref} />
         <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }} transition={{ duration: 0.5, type: "spring" }}>
-            <h3 className='capitalize font-bold text-2xl'>{position}&nbsp; <a href={companyLink} className='text-primary capitalize' target='_blank'>@{company}</a> </h3>
-            <span className='capitalize font-medium text-dark/75'>
+            <h3 className='capitalize font-bold text-2xl'>{position}&nbsp; <a href={companyLink} className='text-primary capitalize dark:text-primaryDark' target='_blank'>@{company}</a> </h3>
+            <span className='capitalize font-medium text-dark/75 dark:text-light/75'>
                 {time} | {address}
             </span>
             <p className='font-medium w-full'>
@@ -37,7 +37,7 @@ const Experience = () => {
 
             <div ref={ref} className='w-[75%] mx-auto relative'>
 
-                <motion.div style={{ scaleY: scrollYProgress }} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top' />
+                <motion.div style={{ scaleY: scrollYProgress }} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light' />
 
                 <ul className='w-full flex flex-col items-start justify-between ml-4'>
                     <Details
