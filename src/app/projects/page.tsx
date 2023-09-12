686d278b7ae822b7b1173e4698e188e99682c6ec
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-12 22:38:42 +0500

    local changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,35 +14,34 @@ const FramerImage = motion(Image);
 const FeaturedProject = ({ type, title, summary, img, link, github }: any) => {
 
     return (
-        <article className='w-full flex items-center justify-between rounded-3xl border border-solid rounded-br-2xl border-dark bg-light shadow-2xl p-12 relative dark:bg-dark dark:border-light'>
-            <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] rounded-br-3xl bg-dark dark:bg-dark  dark:border-light' />
-
-
-
-            <Link href={link} target='_blank' className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
+        <article className='w-full flex items-center justify-between rounded-3xl border border-solid rounded-br-2xl border-dark bg-light shadow-2xl p-12 relative dark:bg-dark dark:border-light
+            lg:flex-col lg:p-8 xs:rounded-2xl  xs:rounded-br-3xl xs:p-4 
+        '>
+            <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] rounded-br-3xl bg-dark dark:bg-dark  dark:border-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]' />
+            <Link href={link} target='_blank' className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full'>
                 <FramerImage src={img} alt={title} className="w-full h-auto"
                     whileHover={{ scale: 1.05 }}
                     transition={{ duration: 0.2 }}
-                    priority 
+                    priority
                     sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw,50vw"
-                />  
+                />
             </Link>
 
-            <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+            <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6 xs:text-base'>
                 <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
 
                 <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-                    <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light'>{title}</h2>
+                    <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm'>{title}</h2>
                     {/* <Image src={img} alt={title} className="w-full h-auto" /> */}
                 </Link>
 
-                <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>
+                <p className='my-2 font-medium text-dark dark:text-light sm:text-sm'>{summary}</p>
 
                 <div className='mt-2 flex items-center'>
                     <Link href={github} target='_blank' className='w-10'>
                         <GithubIcon />
                     </Link>
-                    <Link href={link} target='_blank' className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark'>
+                    <Link href={link} target='_blank' className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base'>
                         Visit Projects
                     </Link>
                 </div>
@@ -57,8 +56,8 @@ const FeaturedProject = ({ type, title, summary, img, link, github }: any) => {
 const Project = ({ title, type, img, link, github }: any) => {
 
     return (
-        <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light'>
-            <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] rounded-br-3xl bg-dark  dark:bg-light' />
+        <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4'>
+            <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] rounded-br-3xl bg-dark  dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]' />
             <Link href={link} target='_blank' className='w-full cursor-pointer overflow-hidden rounded-lg'>
                 <FramerImage src={img} alt={title} className="w-full h-auto"
                     whileHover={{ scale: 1.05 }}
@@ -67,19 +66,19 @@ const Project = ({ title, type, img, link, github }: any) => {
             </Link>
 
             <div className='w-full flex flex-col items-start justify-between mt-4'>
-                <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
+                <span className='text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base'>{type}</span>
 
                 <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-                    <h2 className='my-2 w-full text-left text-3xl font-bold'>{title}</h2>
+                    <h2 className='my-2 w-full text-left text-3xl font-bold lg:text-2xl'>{title}</h2>
                     {/* <Image src={img} alt={title} className="w-full h-auto" /> */}
                 </Link>
 
 
                 <div className='w-full mt-2 flex items-center justify-between'>
-                    <Link href={link} target='_blank' className='text-lg font-semibold'>
+                    <Link href={link} target='_blank' className='text-lg font-semibold underline md:text-base'>
                         Visit
                     </Link>
-                    <Link href={github} target='_blank' className='w-8'>
+                    <Link href={github} target='_blank' className='w-8 md:w-6'>
                         <GithubIcon />
                     </Link>
 
@@ -93,9 +92,9 @@ const page = () => {
     return (
         <section className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
             <Layout className="pt-16">
-                <AnimatedText text="Imagination Trumps Knowledge!" className="mb-6" />
+                <AnimatedText text="Imagination Trumps Knowledge!" className="mb-6 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl" />
 
-                <div className='grid grid-cols-12 gap-24'>
+                <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
                     <div className='col-span-12'>
                         <FeaturedProject
                             title="Crypto Screener Application"
@@ -147,7 +146,6 @@ const page = () => {
                             link="/"
                             type="Featured Project"
                             github="/"
-
                         />
                     </div>
 
