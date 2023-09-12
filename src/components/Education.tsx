0844dfeefb57749617b57892f3d0f0b63d54fc55
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-09-12 19:49:30 +0500

    education and lineLiicon

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -4,10 +4,12 @@ import LineLiicon from './LineLiicon'
 
 const Details = ({ type, time, place, info }: any) => {
     const ref = useRef(null);
-    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]'>
         <LineLiicon reference={ref} />
         <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }} transition={{ duration: 0.5, type: "spring" }}>
-            <h3 className='capitalize font-bold text-2xl'>{type}</h3>
+            <h3 className='capitalize font-bold text-2xl'>
+                {type}
+            </h3>
             <span className='capitalize font-medium text-dark/75'>
                 {time} | {place}
             </span>
@@ -30,20 +32,24 @@ const Education = () => {
 
     return (
         <div className='my-64'>
-            <h2 className='font-bold text-8xl mb-32 w-full text-center'>Education</h2>
+            <h2 className='font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16'>
+                Education
+            </h2>
 
-            <div ref={ref} className='w-[75%] mx-auto relative'>
+            <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
 
-                <motion.div style={{ scaleY: scrollYProgress }} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light' />
+                <motion.div
+                    style={{ scaleY: scrollYProgress }}
+                    className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]'
+                />
 
-                <ul className='w-full flex flex-col items-start justify-between ml-4'>
+                <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
                     <Details
                         type="Bachelor Of Science In Computer Science"
                         time="2016-2020"
                         place="Massachusetts Institute Of Technology (MIT)"
                         info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
                         Intelligence."
-
                     />
                     <Details
                         type="Bachelor Of Science In Computer Science"
--- src/components/LineLiicon.tsx
+++ src/components/LineLiicon.tsx
@@ -19,7 +19,6 @@ const LineLiicon = ({ reference }: any) => {
                 <circle cx="75" cy="50" r="20" className='stroke-primary dark:stroke-primaryDark stroke-1 fill-none' />
                 <motion.circle cx="75" cy="50" r="20" className='stroke-[5px] fill-light dark:fill-dark'
                     style={{ pathLength: scrollYProgress }}
-
                 />
                 <circle cx="75" cy="50" r="10" className='animate-pulse stroke-1 fill-primary dark:fill-primaryDark' />
             </svg>
